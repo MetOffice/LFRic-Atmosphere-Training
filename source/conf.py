083ac419ecc299f418d2f973f963f981a8137568
Kolen Cheung <christian.kolen@gmail.com>
2026-04-23 18:49:11 +0100

    Enable numfig and add figure labels to introduction section
    Enable Sphinx numfig for automatic figure numbering and add
    cross-reference labels to the three figures in the introduction
    section (components, seamless modelling, history/context).
    
    Resolves part of #122.

--- source/conf.py
+++ source/conf.py
@@ -27,6 +27,9 @@ extensions = ['pydata_sphinx_theme',
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- Figure numbering --------------------------------------------------------
+numfig = True
+
 # -- layout -----------------------------------------------------------------
 # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/layout.html#
 
